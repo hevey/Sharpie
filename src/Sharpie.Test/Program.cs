750fa609a8c1bbfc57b202344a5facf169b9a4c6
Glenn Hevey <glenn.hevey@gmail.com>
2020-10-20 23:24:00 +1100

    Added Test Project

---
+++ src/Sharpie.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Sharpie.Models;
+
+namespace Sharpie.Test
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var elements = new P
+            {
+                ChildNode = new TextElement
+                {
+                    Content = "I am child of P1"
+                },
+                SiblingNode = new P
+                {
+                    ChildNode = new TextElement
+                    {
+                        Content = "I am child of P2"
+                    }
+                }
+            };
+
+            var output = elements.Render();
+            Console.WriteLine(output);
+        }
+    }
+}
