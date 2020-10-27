180de1a2e2f02677163ec3b09cbb74b946515013
Glenn Hevey <glenn.hevey@gmail.com>
2020-10-27 20:12:41 +1100

    updated test elements for node checking

--- src/Sharpie/Models/Grouping/P.cs
+++ src/Sharpie/Models/Grouping/P.cs
@@ -4,7 +4,7 @@ public class P : HtmlElement
 {
     private string? _content;
 
-    public override string? Render()
+    public override string Render()
     {
         _content = ChildNode?.Render();
         _content = _content != null ? $"<p{GetAttributes()}>{_content}</p>" : $"<p{GetAttributes()} />";
--- src/Sharpie/Models/Html.cs
+++ src/Sharpie/Models/Html.cs
@@ -1,13 +1,29 @@
+using System;
+using System.Collections.Generic;
+
 namespace Sharpie.Models
 {
     public class Html : HtmlElement
     {
-        public string? xmlns;
+        [Attribute]
+        public string? Xmlns;
         
         private string? _content;
+
+        public Html()
+        {
+            AllowedChildTypes = new List<Node>
+            {
+
+            };
+        }
         
         public override string? Render()
-        { 
+        {
+            if (!IsChildAllowed(ChildNode))
+            {
+                throw new InvalidOperationException("");
+            }
             _content = ChildNode?.Render();
             _content = _content != null ? $"<html>{_content}</html>" : "<html></html>";
         
